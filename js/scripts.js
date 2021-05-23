1bed471cbc5bef5c1372b9cd26923a32e9d47b2b
TA <tamilarasuva444@gmail.com>
2021-05-23 10:08:29 +0530

    side menu bar added

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//active nav item
+$(".nav-item").on("click", function (e) {
+  // Remove class active form all li.nav-tiem
+  $("li.nav-item").removeClass("active");
+  // Add Class to current Element.
+  $(this).addClass("active");
+});
+// close left slide menu after clicking nav-item
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
